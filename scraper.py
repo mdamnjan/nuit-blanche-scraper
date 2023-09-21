073b75f44607c6a5abebad963edfc56ac60cefa7
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-09-20 22:21:14 -0400

    Add ability to write scraped exhibit data to csv

--- scraper.py
+++ scraper.py
@@ -1,13 +1,15 @@
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
+from selenium.common.exceptions import NoSuchElementException
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", False)
 driver = webdriver.Chrome(options=options)
 
 # add an implicit wait to allow the exhibits to load on the page
-driver.implicitly_wait(20)
+driver.implicitly_wait(10)
 driver.get("https://www.toronto.ca/explore-enjoy/festivals-events/nuitblanche/all-art-projects/")
 
 def get_exhibit_links():
@@ -30,31 +32,42 @@ def get_exhibit_links():
 
     return exhibits
    
-def get_exhibit_details(href):
-    driver.get(href)
+def get_exhibit_details(link):
+    driver.get(link)
 
     project_details = driver.find_element(By.CLASS_NAME, "project-details-inner")
+
     title = driver.find_element(By.TAG_NAME, "h1").text
-    print(title)
     exhibit_number = project_details.find_element(By.TAG_NAME, "text").text
-    print(exhibit_number)
-    artists = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Artists')]]").text
-    print(artists)
+    artists = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Artist')]]").text
     medium = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Medium')]]").text
-    print(medium)
-    project_type = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Project Type')]]").text
-    print(project_type)
-    address = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Address')]]").text
-    print(address)
+    address = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Address')]]").text.split('\n')[0]
     physical_access = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Physical Access')]]").text
-    print(physical_access)
+
+    # optional information that may or may not exist
+    project_type = None
+    try: 
+        project_type = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Project Type')]]").text
+    except NoSuchElementException:
+        pass
+
+    return [title, exhibit_number, address, link, artists, medium, physical_access, project_type]
 
 
 exhibit_links = get_exhibit_links()
 
-# exhibit_details = []
-# for link in exhibit_links:
-#     exhibit_details.append(get_exhibit_details(link))
+
+def write_exhibits_to_csv():
+    header = ['title', 'exhibit_number', 'address', 'link', 'artists', 'medium', 'physical_access', 'project_type']
+
+    with open('exhibits.csv', 'w', encoding='UTF8') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+
+        for link in exhibit_links:
+            data = get_exhibit_details(link)
+
+            writer.writerow(data)
 
 
-get_exhibit_details("https://www.toronto.ca/explore-enjoy/festivals-events/nuitblanche/all-art-projects/details/2023/project/translunar-formations-nathan-phillips-square/")
+write_exhibits_to_csv()
