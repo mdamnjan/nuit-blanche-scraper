f2e4fecf37140d2da6ac183a4a0fae00d29575e6
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-09-20 21:39:45 -0400

    Write function for scraping exhibit details from the exhibits page

--- scraper.py
+++ scraper.py
@@ -3,11 +3,11 @@ from selenium.webdriver.common.by import By
 
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
+options.add_experimental_option("detach", False)
 driver = webdriver.Chrome(options=options)
 
 # add an implicit wait to allow the exhibits to load on the page
-driver.implicitly_wait(10)
+driver.implicitly_wait(20)
 driver.get("https://www.toronto.ca/explore-enjoy/festivals-events/nuitblanche/all-art-projects/")
 
 def get_exhibit_links():
@@ -33,5 +33,28 @@ def get_exhibit_links():
 def get_exhibit_details(href):
     driver.get(href)
 
+    project_details = driver.find_element(By.CLASS_NAME, "project-details-inner")
+    title = driver.find_element(By.TAG_NAME, "h1").text
+    print(title)
+    exhibit_number = project_details.find_element(By.TAG_NAME, "text").text
+    print(exhibit_number)
+    artists = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Artists')]]").text
+    print(artists)
+    medium = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Medium')]]").text
+    print(medium)
+    project_type = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Project Type')]]").text
+    print(project_type)
+    address = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Address')]]").text
+    print(address)
+    physical_access = driver.find_element(By.XPATH, "//li[strong[contains(text(), 'Physical Access')]]").text
+    print(physical_access)
+
 
 exhibit_links = get_exhibit_links()
+
+# exhibit_details = []
+# for link in exhibit_links:
+#     exhibit_details.append(get_exhibit_details(link))
+
+
+get_exhibit_details("https://www.toronto.ca/explore-enjoy/festivals-events/nuitblanche/all-art-projects/details/2023/project/translunar-formations-nathan-phillips-square/")
