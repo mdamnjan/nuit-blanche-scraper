c4dd049768fcd4477e2051b165fae4e101c0979b
mdamnjan <mimi.damnjanovic16@gmail.com>
2023-09-20 20:50:15 -0400

    Fetch links to all exhibit pages

--- scraper.py
+++ scraper.py
@@ -10,3 +10,28 @@ driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(10)
 driver.get("https://www.toronto.ca/explore-enjoy/festivals-events/nuitblanche/all-art-projects/")
 
+def get_exhibit_links():
+    exhibits = []
+    has_more_pages = True
+    while has_more_pages:
+        new_exhibits = driver.find_elements(By.XPATH, "//a[contains(@class, 'moreInfo')]")
+
+        for exhibit in new_exhibits:
+            exhibits.append(exhibit.get_attribute('href'))
+
+        next_page_button = driver.find_element(By.CLASS_NAME, "nextPageButton")
+        
+        # the 'next' button on the last page isn't actually disabled for whatever reason
+        # but it has the aria-disabled label
+        if next_page_button.get_attribute('aria-disabled'):
+            has_more_pages = False
+            break
+        next_page_button.click()
+
+    return exhibits
+   
+def get_exhibit_details(href):
+    driver.get(href)
+
+
+exhibit_links = get_exhibit_links()
