c763be4d6434a3fd9d2910ee353945eb2b42f16b
mdamnjan <mimi.damnjanovic16@gmail.com>
2024-04-15 21:10:04 -0400

    Include long, lat coordinates from mapbox geocoding api

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -1,5 +1,10 @@
 import csv
+import os
 import json
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 exhibits = []
 with open('exhibits.csv', newline='') as csvfile:
@@ -7,6 +12,11 @@ with open('exhibits.csv', newline='') as csvfile:
     headers = next(csv_reader)
     for row in csv_reader:
         title, exhibit_number, address, link, artists, medium, physical_access, project_type = row
+
+        # fetch coords from mapbox api
+        res = requests.get('https://api.mapbox.com/geocoding/v5/mapbox.places/{}.json?access_token={}'.format(address, os.getenv('MAPBOX_API_KEY')))
+        response = json.loads(res.text)
+        coordinates = response['features'][0]['geometry']['coordinates']
 
         exhibits.append({
             'title': title,
@@ -16,7 +26,8 @@ with open('exhibits.csv', newline='') as csvfile:
             'artists': artists,
             'medium': medium,
             'physicalAccess': physical_access,
-            'projectType': project_type
+            'projectType': project_type,
+            'coordinates': coordinates
             })
 
 with open("exhibits.json", "w") as outfile:
